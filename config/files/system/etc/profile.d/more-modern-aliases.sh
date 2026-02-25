6bbe94d526d00170b3242108410ae8c68e75c0f0
Doug Elkin <1406429+duhdugg@users.noreply.github.com>
2026-02-24 21:53:10 -0500

    beginning to move away from muhbox container

---
+++ config/files/system/etc/profile.d/more-modern-aliases.sh
@@ -0,0 +1,4 @@
+alias .h='cd $(zoxide query --interactive)'
+alias cat='bat --style plain'
+alias ls='lsd'
+alias lt='lsd -l --tree'
--- config/files/system/etc/profile.d/muhbox.sh
+++
@@ -1,2 +0,0 @@
-alias hostbox='distrobox-host-exec bash -i || echo already on hostbox'
-alias main='distrobox enter muhbox'
